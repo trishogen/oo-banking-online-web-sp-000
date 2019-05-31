c8bf1ba77a36f0fd2893c98179a181b92863af1a
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 01:37:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,11 @@ class Transfer
   end
 
   def reverse_transfer
+    if status = 'complete'
+      self.sender.balance += amount
+      self.receiver.balance += amount
+      self.status = 'reveresed'
+    end
   end
 
 end
