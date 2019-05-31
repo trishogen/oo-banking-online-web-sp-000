01c2cd7a7e2a513a662d781bdb68005bae0e72ac
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 01:30:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     if self.status == 'pending'
-      if self.sender.valid? == true
+      if self.transfer.valid?
         self.sender.balance -= amount
         self.receiver.balance += amount
         self.status = 'complete'
